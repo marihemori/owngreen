c244cc948e691d617255cb595e6d6f9bb4e61100
Mariana <moraismariana83@gmail.com>
2020-03-06 17:52:16 -0300

    App.js

--- gardencraft/src/App.js
+++ gardencraft/src/App.js
@@ -2,9 +2,22 @@ import React from 'react';
 
 function App() {
   return (
-    <div>
-
-    </div>
+   <div>
+    <header>
+      <div>
+        <img class="logo" src="img/gardencraft.png" alt="Logo of the Gardencraft"></img>
+      </div>
+      <div>
+      <li><a href="aboutus.html">About us</a></li>
+      <li><a href="ourproducts.html">Our products</a></li>
+      <li><a href="onsale.html">On sale</a></li>
+      <li> 
+          <input type="text" class="searchbox" placeholder="Search seeds ..."></input>
+          <button type="submit" class="searchbtn"><i class="fas fa-search"></i></button>
+      </li>
+      </div>
+    </header>
+   </div>
   );
 }
 

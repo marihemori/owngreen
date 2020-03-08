9954ec36e4c291d08f9da86619ec8d268701ccf2
Mariana <marianamorais.dev@gmail.com>
2020-03-08 17:44:40 -0300

    :sparkles: Header.js

--- gardencraft/src/Home.js
+++ gardencraft/src/Home.js
@@ -1,10 +1,26 @@
 import React, { Component } from 'react';
 
+
 class Home extends Component {
  render() {
   return (
-   <section>
- 
+   <section className="introduction_Container">
+    <div className="introduction_img">
+     <img class="" src="img/introduction_img.svg" alt="Plants"></img>
+    </div>
+    <div class="introduction_text">
+     <h1>Your garden,</h1><br></br>
+     <h1>where you need it.</h1>
+     <blockquote>
+        <p>Organic seeds, ready for planting in your home.</p>
+        <hr class="hr1"></hr>
+     </blockquote>
+     <div class="home_buttons">
+      <a href="plants.html" class="btn" class="sub_btn" id="sub_btn">Plants</a>
+        
+      <a href="furniture.html" class="btn">Furniture</a> 
+    </div>
+    </div>
    </section>
   )
  }

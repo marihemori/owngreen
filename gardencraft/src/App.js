986282b76e6b6ed25dcf76a92cb5a3cb3a261198
Mariana <marianamorais.dev@gmail.com>
2020-03-08 18:39:42 -0300

    :sparkles: Intro

--- gardencraft/src/App.js
+++ gardencraft/src/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import './styles/global.css'
 import './styles/reset.css'
 import Header from './Header';
+import Home from './Home';
 
 function App() {
   return (
     <body>
       <Header />
+      <Home />
     </body>
   );
 }
--- gardencraft/src/Home.js
+++ gardencraft/src/Home.js
@@ -1,26 +1,38 @@
 import React, { Component } from 'react';
+import './styles/home.css'
+import IntroductionImg from './images/introduction_img.png';
 
+const IntroImg = () => {
+ return (
+  <div className="introduction_img">
+   <img class="" src={IntroductionImg} alt="Plants"></img>
+  </div>
+ )
+}
+
+const IntroText = () => {
+ return (
+  <div class="introduction_text">
+   <h1>Your garden,</h1><br></br>
+   <h1>where you need it.</h1>
+   <blockquote>
+    <p>Organic seeds, ready for planting in your home.</p>
+    <hr class="hr1"></hr>
+   </blockquote>
+   <div class="home_buttons">
+    <a href="plants.html" class="btn">Plants</a>
+    <a href="furniture.html" class="btn">Furniture</a>
+   </div>
+  </div>
+ )
+}
 
 class Home extends Component {
  render() {
   return (
-   <section className="introduction_Container">
-    <div className="introduction_img">
-     <img class="" src="img/introduction_img.svg" alt="Plants"></img>
-    </div>
-    <div class="introduction_text">
-     <h1>Your garden,</h1><br></br>
-     <h1>where you need it.</h1>
-     <blockquote>
-        <p>Organic seeds, ready for planting in your home.</p>
-        <hr class="hr1"></hr>
-     </blockquote>
-     <div class="home_buttons">
-      <a href="plants.html" class="btn" class="sub_btn" id="sub_btn">Plants</a>
-        
-      <a href="furniture.html" class="btn">Furniture</a> 
-    </div>
-    </div>
+   <section className="introduction_container">
+    <IntroImg />
+    <IntroText />
    </section>
   )
  }

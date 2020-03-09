d06dc87b4716d6b226c3f3388ccd1b29f09afdbc
Mariana <moraismariana83@gmail.com>
2020-03-09 16:21:20 -0300

    :boom: Deleted some files

---
+++ src/Header.js
@@ -0,0 +1,48 @@
+import React, { Component } from 'react';
+import './styles/header.css'
+import ImgLogo from './images/gardencraft.png'
+
+const Logo = () => {
+  return (
+    <div className="logo">
+      <img class="imglogo" src={ImgLogo} alt="Logo of the Gardencraft"></img>
+    </div>
+  );
+}
+
+const Menu = () => {
+  return (
+    <div className="header_menu">
+      <ul>
+        <li><a href="aboutus.html">About us</a></li>
+        <li><a href="ourproducts.html">Our products</a></li>
+        <li><a href="onsale.html">On sale</a></li>
+        <li>
+          <input type="text" class="searchbox" placeholder="Search seeds ..."></input>
+          <button type="submit" class="searchbtn"><i class="fas fa-search"></i></button>
+        </li>
+      </ul>
+    </div>
+  )
+}
+
+const HeaderCont = () => {
+  return (
+    <header>
+      <div className="header_container">
+        <Logo />
+        <Menu />
+      </div>
+    </header>
+  )
+}
+
+class Header extends Component {
+  render() {
+    return (
+      <HeaderCont />
+    )
+  }
+}
+
+export default Header;

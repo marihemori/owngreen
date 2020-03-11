e47a78de09a92efec68e7a9ee96e43a93f499cce
Mariana <moraismariana83@gmail.com>
2020-03-11 12:31:05 -0300

    Updates

---
+++ precache-manifest.297d30479144079f5f36dbd3637a3d0f.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4527354785595aae13daf09d8059c032",
+    "url": "/Gardencraft/index.html"
+  },
+  {
+    "revision": "6e11ffccbf9d88a6f4e8",
+    "url": "/Gardencraft/static/css/main.ab30c5d4.chunk.css"
+  },
+  {
+    "revision": "fe6c8de816af7b73a8a6",
+    "url": "/Gardencraft/static/js/2.53f0c54f.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/Gardencraft/static/js/2.53f0c54f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6e11ffccbf9d88a6f4e8",
+    "url": "/Gardencraft/static/js/main.4d2b8abf.chunk.js"
+  },
+  {
+    "revision": "20855b4cb76d9bf6568b",
+    "url": "/Gardencraft/static/js/runtime-main.47d44a21.js"
+  },
+  {
+    "revision": "bb30523a989fade12eeb0fda39a04b34",
+    "url": "/Gardencraft/static/media/fert_img.bb30523a.png"
+  },
+  {
+    "revision": "a23badbcf986e93b4e0b9a379acedaac",
+    "url": "/Gardencraft/static/media/introduction_img.a23badbc.png"
+  },
+  {
+    "revision": "4ba8009443b1b4e3baca5be4c95c5184",
+    "url": "/Gardencraft/static/media/receivenews_img.4ba80094.png"
+  },
+  {
+    "revision": "1b655cfbccbc2306bb6a7b49ee2c65b7",
+    "url": "/Gardencraft/static/media/seeds_img.1b655cfb.png"
+  },
+  {
+    "revision": "d87a56262ef66debe0631db298d98bda",
+    "url": "/Gardencraft/static/media/tools_img.d87a5626.png"
+  }
+]);
